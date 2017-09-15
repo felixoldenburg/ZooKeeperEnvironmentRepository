28bdc177f9dbab0027e5be77ad2d962353b78228
Felix Oldenburg <f.oldenburg@mytaxi.com>
2017-09-16 00:03:20 +0200

    Added Apache License Version 2.0 information

--- src/main/java/com/foldenburg/jones/JonesEnvironmentRepository.java
+++ src/main/java/com/foldenburg/jones/JonesEnvironmentRepository.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.foldenburg.jones;
 
 import com.google.gson.Gson;
@@ -19,7 +35,12 @@ import org.springframework.cloud.config.environment.PropertySource;
 import org.springframework.cloud.config.server.environment.EnvironmentRepository;
 import org.springframework.core.Ordered;
 
-//@ConfigurationProperties("spring.cloud.config.server.jones")
+/**
+ * This is a custom environment repository which reads its configuration from ZooKeeper in the Jones format.
+ * It requires a CuratorFramework instance to work with.
+ *
+ * @author Felix Oldenburg
+ */
 public class JonesEnvironmentRepository implements EnvironmentRepository, Ordered
 {
     private static final Logger LOG = LoggerFactory.getLogger(JonesEnvironmentRepository.class);
