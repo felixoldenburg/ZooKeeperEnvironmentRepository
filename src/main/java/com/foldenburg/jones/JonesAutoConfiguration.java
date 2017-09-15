fd003a67f4c08f01bc8d6f31fbe3e1859757c896
Felix Oldenburg <f.oldenburg@mytaxi.com>
2017-09-15 23:56:15 +0200

    Removed unnecessary config class

--- src/main/java/com/foldenburg/jones/JonesAutoConfiguration.java
+++ src/main/java/com/foldenburg/jones/JonesAutoConfiguration.java
@@ -1,10 +1,42 @@
 package com.foldenburg.jones;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Import({JonesConfiguration.class})
+@Profile("jones")
 public class JonesAutoConfiguration
 {
+    @Value("${spring.cloud.config.server.jones.zookeeper.connectionString}")
+    private String zkConnectionString;
+
+    @Value("${spring.cloud.config.server.jones.zookeeper.sessionTimeout:3600000}")
+    private Integer sessionTimeout;
+
+    @Value("${spring.cloud.config.server.jones.zookeeper.connectTimeout:10000}")
+    private Integer connectTimeout;
+
+    final ExponentialBackoffRetry DEFAULT_RETRY_POLICY = new ExponentialBackoffRetry(1000, 3);
+
+
+    @Bean
+    @ConditionalOnMissingBean(JonesEnvironmentRepository.class)
+    public JonesEnvironmentRepository jonesEnvironmentRepository(CuratorFramework curatorFramework)
+    {
+        return new JonesEnvironmentRepository(curatorFramework);
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean(CuratorFramework.class)
+    public CuratorFramework curatorFramework()
+    {
+        return CuratorFrameworkFactory.newClient(zkConnectionString, sessionTimeout, connectTimeout, DEFAULT_RETRY_POLICY);
+    }
 }
