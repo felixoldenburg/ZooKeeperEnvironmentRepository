1c755930a9d5bf1d562afeb0b52790bebbebc970
Felix Oldenburg <f.oldenburg@mytaxi.com>
2017-09-15 15:21:48 +0200

    Initial project structure with Spring Initializr

---
+++ src/main/java/com/foldenburg/springframework/cloud/config/server/jones/jonesrepository/JonesRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.foldenburg.springframework.cloud.config.server.jones.jonesrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonesRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonesRepositoryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/foldenburg/springframework/cloud/config/server/jones/jonesrepository/JonesRepositoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.foldenburg.springframework.cloud.config.server.jones.jonesrepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JonesRepositoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
